ecfdb02bec7f06264e8899731d43042f3a5f6882
Abusantos <abusomwansantos@gmail.com>
2023-12-10 18:54:06 +0100

    updating the delete

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -2,17 +2,49 @@
 import Prompt from "@models/Prompt";
 import { connectDB } from "@utils/database";
 
-export const GET = async ({ params }) => {
+export const GET = async (req, { params }) => {
   try {
     await connectDB();
 
-    const prompts = await Prompt.find({ creator: params.id }).populate(
-      "creator"
-    );
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt)
+      return new Response("Found not prompt with that Id", { status: 404 });
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
-    return new Response("Found no prompt with that Id", { status: 500 });
+    return new Response("Failed to fetch", { status: 500 });
   }
 };
 // then update it
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+  try {
+    //we connect to db
+    await connectDB();
+    //find the prompt
+    const existingPrompt = await Prompt.findById(params.id);
+    //check if we've the prompt
+    if (!existingPrompt)
+      return new Response("Found not prompt with that Id", { status: 404 });
+
+    //pass the new prompt
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    //then we save the new prompt
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 200 });
+  }
+};
+
 // or delete it
+export const DELETE = async(req, {params})=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
